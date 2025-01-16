a871ed303afb40855e7abe41a4a8be69ba54ac10
Huong <huongnguyen.nth1709@gmail.com>
2025-01-16 18:02:16 +0700

    add getAll, getProfile, deleteProfile routes

--- profile-service/src/main/java/com/mycareportal/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/com/mycareportal/profile/controller/UserProfileController.java
@@ -1,7 +1,13 @@
 package com.mycareportal.profile.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mycareportal.profile.dto.ApiResponse;
@@ -14,15 +20,36 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserProfileController {
     UserProfileService userProfileService;
 
-    @PostMapping("/users")
+    @PostMapping
     ApiResponse<UserProfileResponse> createProfile(@RequestBody ProfileCreationRequest request) {
         return ApiResponse.<UserProfileResponse>builder()
                 .result(userProfileService.createProfile(request))
                 .build();
+    }
+
+    @GetMapping
+    ApiResponse<List<UserProfileResponse>> getAllProfiles() {
+        return ApiResponse.<List<UserProfileResponse>>builder()
+                .result(userProfileService.getAllProfiles())
+                .build();
+    }
+
+    @GetMapping("/{profileId}")
+    ApiResponse<UserProfileResponse> getProfile(@PathVariable String profileId) {
+        return ApiResponse.<UserProfileResponse>builder()
+                .result(userProfileService.getProfile(profileId))
+                .build();
+    }
+
+    @DeleteMapping("/{profileId}")
+    ApiResponse<Void> deleteProfile(@PathVariable String profileId) {
+        userProfileService.deleteProfile(profileId);
+        return ApiResponse.<Void>builder().message("Profile has been deleted").build();
     }
 }
